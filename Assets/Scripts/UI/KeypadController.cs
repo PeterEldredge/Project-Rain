24cefb197e7f5343454635ee3013e195d3df9b70
PeterEldredge <peldredge98@gmail.com>
2019-01-31 00:23:21 -0500

    Added keypad functionality to computer in player's room

---
+++ Assets/Scripts/UI/KeypadController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class KeypadController : MonoBehaviour {
+
+	[SerializeField] Text[] _code;
+
+	public UnityEvent onCorrectCode = new UnityEvent();
+	private void OnCorrectCode()
+	{
+		onCorrectCode.Invoke();
+	}
+
+	private int _currentCodePos = 0;
+	private string _correctCode;
+
+	public void SetCode(string code)
+	{
+		_correctCode = code;
+	}
+
+	public void Clicked(string key)
+	{
+		switch(key)
+		{
+			case "Clear":
+				ClearCode();
+				break;
+			case "Enter":
+				EnterCode();
+				break;
+			default:
+				UpdateCode(key);
+				break;
+		}
+	}
+
+	private void ClearCode()
+	{
+		if(_currentCodePos == 0)
+		{
+			//Possibly put in logic to exit key pad
+		}
+		else
+		{
+			_currentCodePos--;
+			_code[_currentCodePos].text = "-";
+		}
+	}
+
+	private void EnterCode()
+	{
+		string code = "";
+
+		foreach(Text tempText in _code)
+		{
+			code += tempText.text;
+		}
+
+		if (code == _correctCode)
+		{
+			OnCorrectCode();
+			Debug.LogError("Correct code input");
+		}
+		else
+		{
+			Debug.LogError("Incorrect code input");
+		}
+	}
+
+	private void UpdateCode(string key)
+	{
+		if(_currentCodePos < _code.Length)
+		{			
+			_code[_currentCodePos].text = key;
+			_currentCodePos++;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class UIController : MonoBehaviour {
+
+	public UnityEvent onUIOpened = new UnityEvent();
+	public void OnUIOpened()
+	{
+		onUIOpened.Invoke();
+	}
+
+	public UnityEvent onUIClosed = new UnityEvent();
+	public void OnUIClosed()
+	{
+		onUIClosed.Invoke();
+	}
+
+	[SerializeField] private GameObject _screenTint;
+	[SerializeField] private GameObject _keypad;
+	
+	public void KeypadUsed(string code)
+	{
+		_screenTint.SetActive(true);
+		_keypad.SetActive(true);
+
+		_keypad.GetComponent<KeypadController>().SetCode(code);
+
+		OnUIOpened();
+	}
+
+	public void KeypadClosed()
+	{
+		_screenTint.SetActive(false);
+		_keypad.SetActive(false);
+
+		OnUIClosed();
+	}
+}
