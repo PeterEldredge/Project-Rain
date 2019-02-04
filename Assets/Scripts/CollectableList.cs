9752509fc35464dcc537b721a79b6c03a1536911
PeterEldredge <peldredge98@gmail.com>
2019-02-04 00:27:03 -0500

    Saving progress on collectables

---
+++ Assets/Scripts/CollectableList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollectableList : MonoBehaviour {
+
+	private Document[] _documents;
+    public Document[] Documents
+    {
+        get { return _documents; }
+    }
+
+    private Item[] _items;
+    public Item[] Items
+    {
+        get { return _items; }
+    }
+
+    private List<JournalEntry> _journal;
+    public List<JournalEntry> Journal
+    {
+        get { return _journal; }
+    }
+
+    private void Awake()
+    {
+        _documents = new Document[20];
+        _items = new Item[20];
+        _journal = new List<JournalEntry>();
+    }
+}
