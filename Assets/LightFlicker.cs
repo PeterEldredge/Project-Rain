2eaf3bc1e4151b77bfb09ea79b1433b56556c5f8
PeterEldredge <peldredge98@gmail.com>
2019-02-27 00:35:59 -0500

    Initial Light Flicker, planning to add way to weight the values

---
+++ Assets/LightFlicker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+	[SerializeField] private Light _light;
+
+	[SerializeField] private float _minOnTime;
+	[SerializeField] private float _maxOnTime;
+
+	[SerializeField] private float _minOffTime;
+	[SerializeField] private float _maxOffTime;
+
+	private bool _on;
+
+	private void Awake()
+	{
+		_on = _light.enabled;
+
+		StartCoroutine(FlickerRoutine());
+	}
+
+	private IEnumerator FlickerRoutine()
+	{
+		while(true)
+		{
+			float waitTime;
+			if(_on) waitTime = Random.Range(_minOnTime, _maxOnTime);
+			else waitTime = Random.Range(_minOffTime, _maxOffTime);
+
+			yield return new WaitForSeconds(waitTime);
+
+			_on = !_on;
+			_light.enabled = _on;
+			//_light.SetLightDirty();
+		}
+	}
+}
