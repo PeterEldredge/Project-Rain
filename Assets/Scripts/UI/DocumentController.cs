3f28764985825766c75cc9b29af7c39bfd2aba64
PeterEldredge <peldredge98@gmail.com>
2019-04-30 00:31:09 -0400

    Added scroll bars to documents

--- Assets/Scripts/UI/DocumentController.cs
+++ Assets/Scripts/UI/DocumentController.cs
@@ -9,9 +9,17 @@ public class DocumentController : MonoBehaviour
     [SerializeField] private Text _title;
     [SerializeField] private Text _content;
 
+    private LayoutElement _contentLayoutElement;
+
+    private void Awake()
+    {
+        _contentLayoutElement = _content.gameObject.GetComponent<LayoutElement>();
+    }
+
     public void Initialize(Document document)
     {
         _title.text = document.Title;
         _content.text = document.Content;
+        _contentLayoutElement.preferredHeight = _content.preferredHeight;
     }
 }
