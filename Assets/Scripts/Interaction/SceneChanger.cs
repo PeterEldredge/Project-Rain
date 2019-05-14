3dad693ff1d1beb98039d6e9c859af318aac08b4
PeterEldredge <peldredge98@gmail.com>
2019-05-13 23:30:40 -0400

    Added scene changing

---
+++ Assets/Scripts/Interaction/SceneChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] private string _sceneName;
+
+    public void OnUse()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
